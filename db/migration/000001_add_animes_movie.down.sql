298b21f023f19af6e425548efd023e77c8d426fb
Yacine <yacine2700dj@gmail.com>
2023-10-16 13:01:56 +0100

    fix migration files

--- db/migration/000001_add_animes_movie.down.sql
+++ db/migration/000001_add_animes_movie.down.sql
@@ -1,8 +1,16 @@
-DROP TABLE IF EXISTS metas;
-DROP TABLE IF EXISTS anime_meta;
-DROP TABLE IF EXISTS languages;
-DROP TABLE IF EXISTS anime_genre;
-DROP TABLE IF EXISTS genres;
-DROP TABLE IF EXISTS anime_studio;
-DROP TABLE IF EXISTS studios;
-DROP TABLE IF EXISTS anime_movie;
+ALTER TABLE "anime_studio" DROP CONSTRAINT IF EXISTS "anime_studio_studio_id_fkey";
+ALTER TABLE "anime_genre" DROP CONSTRAINT IF EXISTS "anime_genre_genre_id_fkey";
+ALTER TABLE "anime_metas" DROP CONSTRAINT IF EXISTS "anime_metas_anime_id_fkey";
+ALTER TABLE "anime_studio" DROP CONSTRAINT IF EXISTS "anime_studio_anime_id_fkey";
+ALTER TABLE "anime_genre" DROP CONSTRAINT IF EXISTS "anime_genre_anime_id_fkey";
+ALTER TABLE "anime_metas" DROP CONSTRAINT IF EXISTS "anime_metas_language_id_fkey";
+ALTER TABLE "anime_metas" DROP CONSTRAINT IF EXISTS "anime_metas_meta_id_fkey";
+
+DROP TABLE IF EXISTS "anime_metas";
+DROP TABLE IF EXISTS "metas";
+DROP TABLE IF EXISTS "languages";
+DROP TABLE IF EXISTS "anime_genre";
+DROP TABLE IF EXISTS "genres";
+DROP TABLE IF EXISTS "anime_studio";
+DROP TABLE IF EXISTS "studios";
+DROP TABLE IF EXISTS "anime_movie";
--- db/migration/000003_add_sessions.down.sql
+++ db/migration/000003_add_sessions.down.sql
@@ -1,1 +1,3 @@
+ALTER TABLE "sessions" DROP CONSTRAINT IF EXISTS "sessions_username_fkey";
+
 DROP TABLE IF EXISTS "sessions";
