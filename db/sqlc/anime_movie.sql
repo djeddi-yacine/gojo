546b8f1222c719f298e18204c540f1c214508866
Yacine <yacine2700dj@gmail.com>
2023-10-18 18:02:38 +0100

    change sqlc queries and types

--- db/sqlc/anime_movie.sql
+++ db/sqlc/anime_movie.sql
@@ -15,14 +15,20 @@ WHERE id = $1 LIMIT 1;
 -- name: UpdateAnimeMovie :one
 UPDATE anime_movie
 SET
-  original_title = COALESCE(sqlc.narg(original_title), original_title),
-  aired = COALESCE(sqlc.narg(aired), aired),
-  release_year = COALESCE(sqlc.narg(release_year), release_year),
-  duration = COALESCE(sqlc.narg(duration), duration)
+  original_title = COALESCE($2, original_title),
+  aired = COALESCE($3, aired),
+  release_year = COALESCE($4, release_year),
+  duration = COALESCE($5, duration)
 WHERE
-  id = sqlc.arg(id)
+  id = $1
 RETURNING *;
 
 -- name: DeleteAnimeMovie :exec
 DELETE FROM anime_movie 
 WHERE id = $1;
+
+-- name: ListAnimeMovies :many
+SELECT * FROM anime_movie
+WHERE release_year = $1 OR $1 = 0
+LIMIT $2
+OFFSET $3;
