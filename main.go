9f80b6b43d6daf6727232fcb085e9ad37f69321b
Djeddi Yacine <yacine2700dj@gmail.com>
2023-12-28 17:39:24 +0100

    add necessary code for the main.go

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"github.com/dj-yacine-flutter/gojo/worker"
 	"github.com/hibiken/asynq"
 	"github.com/jackc/pgx/v5/pgxpool"
+	sk "github.com/rakyll/statik/fs"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
@@ -79,6 +80,13 @@ func v1Http(config utils.Config, gojo db.Gojo, taskDistributor worker.TaskDistri
 		log.Fatal().Err(err).Msg(err.Error())
 	}
 
+	statikFS, err := sk.New()
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot create Statik files for swagger")
+	}
+	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
+	httpMux.Handle("/swagger/", swaggerHandler)
+
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal().Err(err).Msg("cannot create Gateway listener")
@@ -129,67 +137,3 @@ func queue(config utils.Config, redisOpt asynq.RedisClientOpt, gojo db.Gojo) {
 		log.Fatal().Err(err).Msg("failed to start task processor")
 	}
 }
-
-/* func runGRPCServer(config utils.Config, gojo db.Gojo, taskDistrinbutor worker.TaskDistributor) {
-	server, err := api.NewServer(config, gojo, taskDistrinbutor)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot create GRPC server")
-	}
-
-	gprcLogger := grpc.UnaryInterceptor(shared.GrpcLogger)
-	grpcServer := grpc.NewServer(gprcLogger)
-	pb.RegisterGojoServer(grpcServer, server)
-	reflection.Register(grpcServer)
-
-	listener, err := net.Listen("tcp", config.GRPCServerAddress)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot create gRPC listener")
-	}
-
-	fmt.Printf("\u001b[38;5;50m\u001b[48;5;0m- START gRPC SERVER -AT- %s\u001b[0m\n", listener.Addr().String())
-
-	err = grpcServer.Serve(listener)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot start the gRPC server")
-	}
-}
-
-func runGatewayServer(config utils.Config, gojo db.Gojo, taskDistrinbutor worker.TaskDistributor) {
-	server, err := api.NewServer(config, gojo, taskDistrinbutor)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot create Gateway server")
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	grpcMux := runtime.NewServeMux()
-
-	err = pb.RegisterGojoHandlerServer(ctx, grpcMux, server)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot register Gateway server")
-	}
-
-	mux := http.NewServeMux()
-	mux.Handle("/", grpcMux)
-
-	statikFS, err := sk.New()
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot create statik")
-	}
-	swaggerHandler := http.StripPrefix("/swagger/", http.FileServer(statikFS))
-	mux.Handle("/swagger/", swaggerHandler)
-
-	listener, err := net.Listen("tcp", config.HTTPServerAddress)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot create Gateway listener")
-	}
-
-	fmt.Printf("\u001b[38;5;50m\u001b[48;5;0m- START HTTP SERVER -AT- %s\u001b[0m\n", listener.Addr().String())
-
-	handler := shared.HttpLogger(mux)
-	err = http.Serve(listener, handler)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot start the Gateway server")
-	}
-} */
