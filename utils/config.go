944ec1db292a0e40528236cd05e432af405e3d9a
Djeddi Yacine <yacine2700dj@gmail.com>
2023-12-28 20:40:49 +0100

    remove the MIGRATION_URL env as we don't need it anymore

--- utils/config.go
+++ utils/config.go
@@ -9,7 +9,6 @@ import (
 type Config struct {
 	Environment          string        `mapstructure:"ENVIRONMENT"`
 	DBSource             string        `mapstructure:"DB_SOURCE"`
-	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
 	HTTPServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
 	GRPCServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
 	RedisQueueAddress    string        `mapstructure:"REDIS_QUEUE_ADDRESS"`
