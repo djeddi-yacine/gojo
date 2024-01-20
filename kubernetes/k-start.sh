27b7695f4045b3e3d8d8526c33faca799ac17fdc
Djeddi Yacine <yacine2700dj@gmail.com>
2024-01-20 17:16:01 +0100

    add cert-manager to the cluster

--- kubernetes/k-start.sh
+++ kubernetes/k-start.sh
@@ -9,6 +9,11 @@ apply_manifest() {
     fi
 }
 
+kubectl apply -f "https://github.com/cert-manager/cert-manager/releases/download/v1.13.3/cert-manager.yaml"
+
+# Apply gojo-api-cert
+apply_manifest "cluster-issuer.yaml"
+
 # Apply gojo-ingrees
 apply_manifest "ingress-nginx.yaml"
 apply_manifest "ingress-http.yaml"
